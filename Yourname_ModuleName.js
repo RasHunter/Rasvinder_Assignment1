572b18f3a814ffa345eb6c6ede9e89b84665ec92
RasHunter <rasvindersingh31@gmail.com>
2023-11-08 15:29:28 +0800

    Update Yourname_ModuleName.js

--- Yourname_ModuleName.js
+++ Yourname_ModuleName.js
@@ -6,5 +6,14 @@ module.exports = {
     // Explain what function B does
     functionB() {
         console.log("Hello function B");
-    }
+    },
+    functionC() {
+        return 1+2;
+    },
+    functionD() {
+        return 1+2;
+    },
+    functionE() {
+        return 1+2;
+    },
 }
