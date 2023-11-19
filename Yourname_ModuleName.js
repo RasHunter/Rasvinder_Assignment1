db610034004a8940560c3abb78a2d247315435eb
RasHunter <rasvindersingh31@gmail.com>
2023-11-19 19:47:46 +0800

    Update Yourname_ModuleName.js

--- Yourname_ModuleName.js
+++ Yourname_ModuleName.js
@@ -1,47 +1,45 @@
 // module.exports = {
 let karaoke = {
 
-
-    customers : [],
-    // {name:"" ,
-    //     groupsize:"",
-    //     date:"",
-    //     starttime:"",
-    //     endtime:"",
-    // // AddCustomer() {
-    // //     await(this.customers.push({}));
-    // },
-
-    // let customers ={
-    //     name:"" ,
-    //     groupsize:"",
-    //     date:"",
-    //     starttime:"",
-    //     endtime:"",
-        AddCustomer(n,size,date,start,end) {
+    customers : [{name:n ,
+        groupsize:size,
+        date:date,
+        starttime:start,
+        endtime:end,
+        room:r,
+    }],
+    
+        AddCustomer(n,size,date,start,end,r) {
             await(this.customers.push({name:n ,
             groupsize:size,
             date:date,
             starttime:start,
             endtime:end,
+            room:r,
         }))
         console.log("Customer has been added");
         },
     // },
-        Rooms:[],
+        Rooms:[{name:"" ,slots:""}],
 
-        AddRoom(n,c,s) {   
+        AddRooms(){
+            await(this.Room.push({name:"Small" ,slots:2},{name:"Large",slots:6},{name:"Medium",slots:4}));
+            console.log("Rooms added!");
+        },
+        //Preset Rooms to use
+
+        AddRoom(n,s) {   
             
-            await(this.Room.push({name:n , code:c, slots:s}));
+            await(this.Room.push({name:n ,slots:s}));
             console.log("Added Room " + n);
         },
         GetRooms(conditions) {
-            
+            return this.Rooms;
         },
     
 
     
-    // Explain what function B does
+    
     AssignRoom() {
         console.log("Hello function B");
     },
@@ -49,8 +47,7 @@ let karaoke = {
     FindByTime() {
         return 1+2;
     },
-    StopSession() {
-        return 1+2;
+    RemoveCustomer() {
+        
     },
-//},
 }
