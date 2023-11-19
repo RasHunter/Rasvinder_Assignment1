f1e6f80175abacb00e5749e67da01d2799423517
RasHunter <rasvindersingh31@gmail.com>
2023-11-19 20:42:22 +0800

    Finishing

--- README.md
+++ README.md
@@ -7,3 +7,42 @@ In this readme file, describe how to use your node module. It could be similar t
 You can press **Ctrl+Shift+V** in this file in Visual Studio Code to see a live preview of the readme file.
 
 For some tips in formatting text in readme file, refer to https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
+
+The starting is to use the AddRooms function to show the current presets.
+
+karaoke.AddRooms();
+//Gets All the Presets into the Rooms[]
+
+Then to add a new Room if need use AddRoom fuction to add a new room.
+
+karaoke.AddRoom("Very Large",8);
+
+To show all rooms use the function GetRooms
+
+console.log(karaoke.GetRooms());
+
+
+Next would be to Add a Customer
+
+karaoke.AddCustomer("Mariah Carey",4);
+or with room assigning
+karaoke.AddCustomer("Mariah Carey",4,"Medium");
+
+to show all customer use the GetCustomers function
+
+console.log(karaoke.GetCustomers());
+
+if room not assigned by using AddCustomer then the AssignRoom Function can be done.
+
+karaoke.AssignRoom("Mariah Carey");
+//Auto Assign to available room with slots
+
+OR
+
+karaoke.AssignRoom("Mariah Carey","Medium");
+//Manually Assign Rooms
+
+Lastly Customer can be Removed and the room they were in regains its slots by using the function RemoveCustomer
+
+karaoke.RemoveCustomer();
+
--- Yourname_ModuleName.js
+++ Yourname_ModuleName.js
@@ -3,51 +3,127 @@ let karaoke = {
 
     customers : [{name:n ,
         groupsize:size,
-        date:date,
-        starttime:start,
-        endtime:end,
         room:r,
     }],
     
-        AddCustomer(n,size,date,start,end,r) {
-            await(this.customers.push({name:n ,
-            groupsize:size,
-            date:date,
-            starttime:start,
-            endtime:end,
-            room:r,
-        }))
-        console.log("Customer has been added");
-        },
-    // },
-        Rooms:[{name:"" ,slots:""}],
+    async AddCustomer(n,size,r = null) {
+        await(this.customers.push({name:n ,
+        groupsize:size,
+        room:r,
+    }))
+    console.log('Customer has been added');
+    },
+    //Add Customer 
 
-        AddRooms(){
-            await(this.Room.push({name:"Small" ,slots:2},{name:"Large",slots:6},{name:"Medium",slots:4}));
-            console.log("Rooms added!");
+    GetCustomers(){
+        return this.customers;
+    },
+    //Get all Customers
+
+    GetCustomerByRoom(roomName){
+        const customersInRoom = this.customers.filter(customer => customer.room === roomName);
+
+        if (customersInRoom.length > 0) {
+            console.log('Customers in Room ${roomName}:');
+            customersInRoom.forEach(customer => {
+                console.log('- ${customer.name}');
+            });
+        } else {
+            console.log('No customers found in Room ${roomName}');
+        }
+
+        return customersInRoom;
+    },
+    //Get customer inside room
+
+
+
+    Rooms:[{name:"" ,slots:""}],
+
+        async AddRooms(){
+            await(this.Rooms.push({name:"Small" ,slots:2},{name:"Large",slots:6},{name:"Medium",slots:4}));
+            console.log('Rooms added!');
         },
         //Preset Rooms to use
 
-        AddRoom(n,s) {   
+        async AddRoom(n,s) {   
             
-            await(this.Room.push({name:n ,slots:s}));
-            console.log("Added Room " + n);
+            await(this.Rooms.push({name:n ,slots:s}));
+            console.log('Added Room' + n);
         },
+        //Add a new room
+
         GetRooms(conditions) {
             return this.Rooms;
         },
+        //Show all the Rooms
     
+    AssignRoom(customerName) {
+
+        const customer = this.customers.find(cust => cust.name === customerName);
+
+        if (!customer) {
+            console.log(`Customer with name '${customerName}' not found`);
+            return;
+        }
+
+        // Find the first available room
+        const availableRoom = this.Rooms.find(room => room.slots > 0);
+
+        if (availableRoom) {
+            // Assign the room to the customer
+            customer.room = availableRoom.name;
+            availableRoom.slots--;
+
+            console.log(`Room ${availableRoom.name} assigned to ${customer.name}`);
+        } else {
+            console.log("No available rooms");
+        }
+    },
+    //Auto Assign the room by Group size
+    AssignRoom(customerName, roomName) {
+        const customer = this.customers.find(cust => cust.name === customerName);
+
+        if (!customer) {
+            console.log(`Customer with name '${customerName}' not found`);
+            return;
+        }
+
+        const selectedRoom = this.Rooms.find(room => room.name === roomName);
+
+        if (!selectedRoom) {
+            console.log(`Room with name '${roomName}' not found`);
+            return;
+        }
+
+        // Assign the specified room to the customer
+        customer.room = selectedRoom.name;
+        selectedRoom.slots--;
+
+        console.log(`Room ${selectedRoom.name} assigned to ${customer.name}`);
+    },
+    //Manually Assign Customer Room
+
 
     
-    
-    AssignRoom() {
-        console.log("Hello function B");
+    RemoveCustomer(customerName) {
+        const index = this.customers.findIndex(customer => customer.name === customerName);
+
+        if (index !== -1) {
+            const removedCustomer = this.customers.splice(index, 1)[0];
+            console.log(`Customer ${removedCustomer.name} removed`);
+            
+            // If the customer had a room assigned, increase the available slots for that room
+            if (removedCustomer.room) {
+                const assignedRoom = this.Rooms.find(room => room.name === removedCustomer.room);
+                if (assignedRoom) {
+                    assignedRoom.slots++;
+                    console.log(`Room ${assignedRoom.name} slots increased`);
+                }
+            }
+        } else {
+            console.log(`Customer with name '${customerName}' not found`);
+        }
     },
-    
-    FindByTime() {
-        return 1+2;
-    },
-    RemoveCustomer() {
-        
-    },
+    //Removes Customer and returns Rooms slots
 }
